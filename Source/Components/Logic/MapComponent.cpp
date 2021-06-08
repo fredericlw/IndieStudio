cd187104ee45ce0f2fcbfe719ee0b51e35c33783
Leo Fabre <leo4.fabre@epitech.eu>
2021-06-08 19:04:38 +0200

    [+] Map floors and walls fixed

--- Source/Components/Logic/MapComponent.cpp
+++ Source/Components/Logic/MapComponent.cpp
@@ -26,28 +26,19 @@ MapComponent::~MapComponent()
 void MapComponent::init()
 {
     Component::init();
-    //generate walls
     transform = &entity->getComponent<TransformComp>();
     if (!transform)
         transform = &entity->addComponent<TransformComp>(-13, -28, -26);
-    auto newEnt = &entity->_mgr.addEntity(
-        "puto");
-    newEnt->addGroup(GroupLabel::Walls);
-    newEnt->addComponent<TransformComp>(transform->position);
-    newEnt->addComponent<BasicCubeComp>(
-        Vector3D::One().Multiply(2), Colors::RayWhite, Colors::Black);
-    for (int x = cubesize; x < (_size.x - 1) * cubesize; x += static_cast<int>(cubesize)) {
-        for (int z = cubesize; z < (_size.y - 1) * cubesize; z += static_cast<int>(cubesize)) {
-            auto newEnt = &entity->_mgr.addEntity(
-                "Wall:" + std::to_string(x) + ":" + std::to_string(z));
-            newEnt->addGroup(GroupLabel::Walls);
-            newEnt->addComponent<TransformComp>(x + transform->position.x, transform->position.y, transform->position.z + z);
-            newEnt->addComponent<BasicCubeComp>(
-                Vector3D::One().Multiply(2), Colors::RayWhite, Colors::Black);
-            Walls.emplace_back(newEnt);
-        }
-    }
+//    place_root_visualizer();
+    gen_floor();
+    //generate walls
+    gen_walls();
     //generate  obstacles
+    gen_obstacles();
+}
+
+void MapComponent::gen_obstacles()
+{
     for (int i = 0; i < numObstacles; ++i) {
         Vector3D pos = Vector3D::Zero();
         do {
@@ -59,17 +50,66 @@ void MapComponent::init()
                 pos.z = (float) Random::Range(0, (int) _size.y - 1);
         } while (std::find_if(
             Walls.begin(), Walls.end(),
-            [pos](const Entity *ent) {return ent->getComponent<TransformComp>().position == pos;}) != Walls.end());
-            auto newEnt = &entity->_mgr.addEntity(
-                "Obstacle:" + std::to_string(pos.x) + ":" +
-                    std::to_string(pos.z));
-            newEnt->addGroup(GroupLabel::Walls);
-            newEnt->addComponent<TransformComp>(
-                pos.x, transform->position.y, pos.z);
-            newEnt->addComponent<BasicCubeComp>(
-                Vector3D::One().Multiply(2), Colors::RayWhite, Colors::Black);
-            Obstacles.emplace_back(newEnt);
+            [pos](const Entity *ent) {
+                return ent->getComponent<TransformComp>().position == pos;
+            }) != Walls.end());
+        auto newEnt = &entity->_mgr.addEntity(
+            "Obstacle:" + std::to_string(pos.x) + ":" +
+                std::to_string(pos.z));
+        newEnt->addGroup(GroupLabel::Obstacles);
+        newEnt->addComponent<TransformComp>(
+            pos.x, transform->position.y, pos.z);
+        newEnt->addComponent<BasicCubeComp>(
+            Vector3D::One().Multiply(2), RayWhite, Black);
+        Obstacles.emplace_back(newEnt);
     }
+}
+
+void MapComponent::gen_walls()
+{
+    for (int x = cubesize;
+        x < (_size.x - 1) * cubesize; x += static_cast<int>(cubesize * 2)) {
+        for (int z = cubesize;
+            z < (_size.y - 1) * cubesize; z += static_cast<int>(cubesize * 2)) {
+            auto newEnt = &entity->_mgr.addEntity(
+                "Wall:" + std::to_string(x) + ":" + std::to_string(z));
+            newEnt->addGroup(GroupLabel::Walls);
+            newEnt->addComponent<TransformComp>(x + transform->position.x,
+                transform->position.y, transform->position.z + z);
+            newEnt->addComponent<BasicCubeComp>(
+                Vector3D::One().Multiply(2), RayWhite, Black);
+            Walls.emplace_back(newEnt);
+        }
+    }
+}
+
+void MapComponent::gen_floor()
+{
+    for (int x = 0;
+        x < (_size.x) * cubesize;
+        x += static_cast<int>(cubesize)) {
+        for (int z = 0;
+            z < (_size.y) * cubesize;
+            z += static_cast<int>(cubesize)) {
+            auto newEnt = &entity->_mgr.addEntity(
+                "Wall:" + std::to_string(x) + ":" + std::to_string(z));
+            newEnt->addGroup(GroupLabel::Floor);
+            newEnt->addComponent<TransformComp>(x + transform->position.x,
+                transform->position.y - cubesize, transform->position.z + z);
+            newEnt->addComponent<BasicCubeComp>(
+                Vector3D::One().Multiply(2), Black, Black);
+        }
+    }
+}
+
+void MapComponent::place_root_visualizer()
+{
+    auto newEnt = &entity->_mgr.addEntity(
+        "root_visualiser");
+    newEnt->addGroup(GroupLabel::Walls);
+    newEnt->addComponent<TransformComp>(transform->position);
+    newEnt->addComponent<BasicCubeComp>(
+        Vector3D::One().Multiply(2), Green, Black);
 }
 
 void MapComponent::update()
