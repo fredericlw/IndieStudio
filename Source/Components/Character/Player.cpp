b658b43f8518a23ff954654c47e1227ada42bb17
Leo Fabre <fabre.leo@gmail.com>
2021-06-08 17:14:46 +0200

    [+] Model testing

--- Source/Components/Character/Player.cpp
+++ Source/Components/Character/Player.cpp
@@ -33,7 +33,7 @@ void Player::init()
     Component::init();
     _mc = &entity->addComponent<MovementComp>(_eType, _playerNum);
     _model =
-        &entity->addComponent<ModelComp>("./rsc/Models/mrfixit.iqm",
+        &entity->addComponent<ModelComp>("./rsc/Models/bomberman.obj",
             _color,.5);
     _model->rotate({90, 0, 0});
 }
