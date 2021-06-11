beb3412fa8e9a1749ebc01738318bb2172d81267
Thomas Meurice <thomas.meurice@epitech.eu>
2021-06-11 21:58:40 +0200

    Add GameOverComp

---
+++ Include/Components/GUI/GameOverComp.hpp
@@ -0,0 +1,14 @@
+//
+// Created by jupi on 11/06/2021.
+//
+
+#ifndef BOMBERMAN_GAMEOVERCOMP_H
+#define BOMBERMAN_GAMEOVERCOMP_H
+
+
+class GameOverComp {
+
+};
+
+
+#endif //BOMBERMAN_GAMEOVERCOMP_H
---
+++ Source/Components/GUI/GameOverComp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jupi on 11/06/2021.
+//
+
+#include "Components/GUI/GameOverComp.hpp"
