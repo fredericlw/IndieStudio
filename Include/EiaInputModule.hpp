336853bfc8d7055c15076254fc27ea66858fa8c7
Adrien <adrien.courbebaisse@epitech.eu>
2021-06-18 17:13:59 +0200

    ai dev stuff

---
+++ Include/EiaInputModule.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** bomberman
+** File description:
+** Created by Adrien Courbebaisse
+*/
+#ifndef EIAINPUTMODULE_HPP
+#define EIAINPUTMODULE_HPP
+
+#include "AInputModule.hpp"
+#include "Entity.hpp"
+
+class EIAInputModule : public AInputModule {
+public:
+    EIAInputModule(
+        int gamepad_nbr, Entity *entity
+    );
+    ~EIAInputModule();
+    bool GetButtonDown(Button btn) override;
+    bool GetButtonUp(Button btn) override;
+    bool GetButtonPressed(Button btn) override;
+    bool GetButtonReleased(Button btn) override;
+private:
+    size_t _playerNum;
+    std::vector<std::shared_ptr<Entity>> otherPLayers;
+};
+
+#endif //EIAINPUTMODULE_HPP
---
+++ Source/raylib_encap/EiaInputModule.cpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2023
+** bomberman
+** File description:
+** Created by Adrien Courbebaisse
+*/
+#include <EiaInputModule.hpp>
+
+EIAInputModule::EIAInputModule(int gamepad_nbr)
+{
+}
+
+EIAInputModule::~EIAInputModule()
+{
+}
+
+bool EIAInputModule::GetButtonDown(Button btn)
+{
+    return false;
+}
+
+bool EIAInputModule::GetButtonUp(Button btn)
+{
+    return false;
+}
+
+bool EIAInputModule::GetButtonPressed(Button btn)
+{
+    return false;
+}
+
+bool EIAInputModule::GetButtonReleased(Button btn)
+{
+    return false;
+}
+
